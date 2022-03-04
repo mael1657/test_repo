eb8b1196b4e3c9f6a2a1f51debc2741045ef91ff
mael1657 <jhmael013@gmail.com>
2022-03-04 15:31:28 +0900

    린트, 프리티어 세팅

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,16 @@
-// .prettierrc.js
 module.exports = {
+  // 문자열은 홀따옴표(')로 formatting
   singleQuote: true,
-  // 문자열은 홑따옴표로 formatting
-  semi: true,
   //코드 마지막에 세미콜른이 있게 formatting
-  useTabs: false,
+  semi: true,
   //탭의 사용을 금하고 스페이스바 사용으로 대체하게 formatting
-  tabWidth: 2,
+  useTabs: false,
   // 들여쓰기 너비는 2칸
+  tabWidth: 2,
+  // 객체나 배열을 작성 할 때, 원소 혹은 key-valueㅇ의 맨 뒤에 있는 것에도 쉼표를 붙임
   trailingComma: 'all',
-  // 자세한 설명은 구글링이 짱이긴하나 객체나 배열 키:값 뒤에 항상 콤마를 붙히도록 	  	//formatting
-  printWidth: 80,
   // 코드 한줄이 maximum 80칸
-  arrowParens: 'avoid',
+  printWidth: 80,
   // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략하게 formatting
-  endOfLine: "auto",
-  // windows에 뜨는 'Delete cr' 에러 해결
+  arrowParens: 'avoid',
 };
